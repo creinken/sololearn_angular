c90b41cdbce639377b626b9f06f17862f9484f79
creinken <reinkendigital@gmail.com>
2021-01-19 18:49:10 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-z5k9u3
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-z5k9u3)
---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, VERSION } from "@angular/core";
+import { TransportationService } from "./transportation.service";
+import { Car } from "./car";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  cars: Car[];
+
+  constructor(private transportationService: TransportationService) {
+    this.cars = this.transportationService.getCars();
+  }
+
+  counter = 0;
+  increment() {
+    this.counter++;
+  }
+
+  phrase = "It's going";
+  update() {
+    this.phrase += " ..and going";
+  }
+}
